05a7bf2e9713802dc6b697aea5bfc88c63bb3976
Bernadette Davis <bsd231@nyu.edu>
2020-02-04 17:02:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let BoardMember = function(name, homeState, training){
 };
 
 BoardMember.prototype.veto = function(){
-  returns `No, I must disagree`;
+  return `No, I must disagree`;
 };
 BoardMember.prototype.approve = function(){
   return `You can do that!`;
