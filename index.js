de990fd427c38ad0d954e329920407e1906804b9
Bernadette Davis <bsd231@nyu.edu>
2020-02-04 16:33:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+let BoardMember = function(name, homeState, training){
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+};
+
+BoardMember.prototype.veto = function(){
+  returns "`
+};
+BoardMember.prototype.approve = function(){};
+BoardMember.prototype.doCharity = function(){};
+BoardMember.prototype.releasePressStatement = function(){};
+BoardMember.prototype.sayHi = function(){};
